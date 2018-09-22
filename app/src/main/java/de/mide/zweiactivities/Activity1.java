fc22a95e3555da57780d9eca4e1cec76f8885eb1
mide42 <demi_05@arcor.de>
2018-09-22 22:01:22 +0200

    Activity2 gibt jetzt an Activity1 Extra zurueck

--- app/src/main/java/de/mide/zweiactivities/Activity1.java
+++ app/src/main/java/de/mide/zweiactivities/Activity1.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 
 /**
@@ -20,6 +21,10 @@ public class Activity1 extends Activity
     /** UI-Elemente zur Eingabe von Text durch den Nutzer. */
     protected EditText _editText = null;
 
+    /** UI-Element zur Anzeige von Text, der von Activity2 zurückgegeben wird. */
+    protected TextView _textView = null;
+
+
     /**
      * Nach Laden der Layout-Datei und Füllen der Member-Variablen mit Referenzen auf UI-Elemente
      * wird noch die Activity-Instanz selbst ("this") als Event-Handler für den Button
@@ -31,6 +36,8 @@ public class Activity1 extends Activity
         setContentView(R.layout.activity_1);
 
         _editText = findViewById(R.id.editTextAufActivity1);
+
+        _textView = findViewById(R.id.textViewAufActivity1);
 
         Button button = findViewById(R.id.buttonAufActivity1);
         button.setOnClickListener(this);
@@ -52,7 +59,35 @@ public class Activity1 extends Activity
         String textToTransfer = _editText.getText().toString();
         intent.putExtra("data_von_1_zu_2", textToTransfer);
 
-        startActivity(intent);
+        //startActivity(intent);
+        startActivityForResult(intent, 123);
+    }
+
+
+    /**
+     * Callback-Methode für Rückkehr der Activity 2. Damit diese Methode aufgerufen wird,
+     * muss der Intent zum Aufruf der Activity 2 mit "startActivityForResult()" statt mit
+     * "startActivity()" abgesetzt werden.
+     *
+     * @param requestCode  Code, anhand dem wir verschiedene "Rückkehrer" unterscheiden können
+     *                     (in diesem Beispiel gibt es aber nur einen Rückkehrer).
+     *
+     * @param resultCode   Code an dem wir erkennen können, ob die aufgerufene Activity erfolgreich
+     *                     beendet wurde.
+     *
+     * @param intent       Intent-Objekt, das von Activity 2 erzeugt wurde und den "Rückgabe-Wert"
+     *                     enthält.
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+
+        if (resultCode != RESULT_OK) {
+            _textView.setText("Activity 2 wurde abgebrochen");
+            return;
+        }
+
+        String textReceived = intent.getStringExtra("data_von_2_zu_1");
+        _textView.setText(textReceived);
     }
 
 }
--- app/src/main/java/de/mide/zweiactivities/Activity2.java
+++ app/src/main/java/de/mide/zweiactivities/Activity2.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 /**
@@ -14,12 +15,14 @@ import android.widget.TextView;
  *
  * This project is licensed under the terms of the BSD 3-Clause License.
  */
-
 public class Activity2 extends Activity
                        implements View.OnClickListener {
 
-    /** UI-Element zur Anzeige von Text, der auf der anderen Activity eingegeben wurde. */
+    /** UI-Element zur Anzeige von Text, der auf Activity1 eingegeben wurde. */
     protected TextView _textView = null;
+
+    /** UI-Element für Eingabe von Text, der an Activity1 zurückgegeben werden soll. */
+    protected EditText _editText = null;
 
 
     /**
@@ -33,6 +36,8 @@ public class Activity2 extends Activity
         setContentView(R.layout.activity_2);
 
         _textView = findViewById(R.id.textViewAufActivity2);
+
+        _editText = findViewById(R.id.editTextAufActivity2);
 
         Button button = findViewById(R.id.buttonAufActivity2);
         button.setOnClickListener(this);
@@ -54,6 +59,14 @@ public class Activity2 extends Activity
      */
     @Override
     public void onClick(View view) {
+
+        String textToTransfer = _editText.getText().toString();
+
+        Intent intent = new Intent();
+        intent.putExtra("data_von_2_zu_1", textToTransfer);
+        setResult(RESULT_OK, intent);
+        //setResult(RESULT_CANCELED, intent);
+
         finish();
     }
 
